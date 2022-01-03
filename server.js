53299f4be1c2de2f1f4f73e4d8c35121e55e0f7b
Yanti S <67404342+macalada@users.noreply.github.com>
2022-01-03 11:34:33 -0700

    back end of smartbrain app

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+const knex = require('knex')
+
+const register = require('./controllers/register');
+const signIn = require('./controllers/signIn');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password :'stefan',
+      database : 'smartbrain'
+    }
+  });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', (req, res)=>res.json('success'))
+app.get('/profiles/:id', (req, res)=> profile.handleProfile(req, res, db))
+app.put('/image', (req, res)=> image.handleImage(req, res, db))
+app.post('/imageurl', (req, res)=> image.handleApiCall(req, res))
+app.post('/signin',(req, res)=> signIn.handleSignIn(req, res, db, bcrypt) )
+app.post('/register', (req, res)=> register.handleRegister(req, res, db, bcrypt))
+
+app.listen(4000, ()=>{
+    console.log('listening to port 4000')
+})
