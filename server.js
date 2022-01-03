96ab8dd5fee142153acf92592893fa8e57021c26
Yanti S <67404342+macalada@users.noreply.github.com>
2022-01-03 12:26:39 -0700

    adding db conf

--- server.js
+++ server.js
@@ -12,11 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      port : 5432,
-      user : 'postgres',
-      password :'stefan',
-      database : 'smartbrain'
+      connectionString : process.env.DATABASE_URL,
+      ssl:true,
+
     }
   });
 
